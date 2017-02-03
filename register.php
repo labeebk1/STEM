8eb6e19fe18469e27e69b9806214d38b86bf2ba4
labeebk1 <labeebk1@gmail.com>
2017-02-02 22:48:40 -0500

    1

--- register.php
+++ register.php
@@ -9,7 +9,7 @@
 			$username = $mysqli->real_escape_string($_POST['username']);
 			$email = $mysqli->real_escape_string($_POST['email']);
 			$password = md5($_POST['password']);
-			$sql = "INSERT INTO users (username, email, password) VALUES ('$username','$email','$password')";
+			$sql = "INSERT INTO STEM.users (username, email, password) VALUES ('$username','$email','$password')";
 
 			if ($mysqli->query($sql) == true){
 				$_SESSION['message'] = 'Registration Successful! Added $username to the Instructor Database!';
