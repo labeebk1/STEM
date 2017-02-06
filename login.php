35d667cdda79c90ab522c8b8cd529f27cca56c7b
labeebk1 <labeebk1@gmail.com>
2017-02-06 12:44:11 -0500

    t

--- login.php
+++ login.php
@@ -14,6 +14,7 @@
 		$rows = $result->num_rows;
     	if($rows > 0){
 			$_SESSION['message'] = "Login Successful!";
+			$_SESSION['username']= $username;
             header("Location:portal.php");
     	} else {
 			$_SESSION['message'] = "Incorrect Username or Password";
