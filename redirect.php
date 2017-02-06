faedc604a5153f25f91d251e4b5c81c687cc43af
labeebk1 <labeebk1@gmail.com>
2017-02-06 15:14:36 -0500

    test

--- redirect.php
+++ redirect.php
@@ -1,15 +1,12 @@
 
 <?php
-	header('refresh: 10; url=login.php');
+	header('refresh: 3; url=login.php');
 ?>
-
 <!DOCTYPE html>
 <html>
 <body>
-
 <?php
 echo "Error! You are not logged in. Redirecting in 3 seconds...";
 ?> 
-
 </body>
 </html>
