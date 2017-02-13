486ce8b3d969615f07e32e819b22e3f74a2e6468
labeebk1 <labeebk1@gmail.com>
2017-02-13 00:53:11 -0500

    test

--- functions.php
+++ functions.php
@@ -35,9 +35,9 @@ function getCalender($year = '',$month = '')
 ?>
 
 	<div id="calender_section">
-		<h1>
-			hey
-		</h1>
+		<legend> 
+			
+		</legend>
 		<h2 style="background-color: white";>
         	<a href="javascript:void(0);" onclick="getCalendar('calendar_div','<?php echo date("Y",strtotime($date.' - 1 Month')); ?>','<?php echo date("m",strtotime($date.' - 1 Month')); ?>');">&lt;&lt;</a>
             <select name="month_dropdown" class="month_dropdown dropdown"><?php echo getAllMonths($dateMonth); ?></select>
