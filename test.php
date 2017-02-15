47d8be66505d818475c781f98e70453822df7055
labeebk1 <labeebk1@gmail.com>
2017-02-14 23:42:16 -0500

    test

---
+++ test.php
@@ -0,0 +1,275 @@
+
+<?php
+  session_start();
+  if(empty($_SESSION['username'])){
+    header("Location:login.php");
+  }
+?>
+
+
+
+<html lang="en">
+
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+</head>
+
+
+<body>
+  <br>
+  <br>
+  <h1 style="font-size: 20px; text-align: center;"> <b>STEM Academy Instructor Portal </b></h1>
+  <br>
+
+    <nav>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li class="active"><a href="portal.php">Dashboard</a></li>
+        <li><a href="calendar.php">Calendar</a></li><!-- <span class="badge">4</span> -->
+        <li><a href="portal.php">Messages<span class="badge green">8</span></a></li>
+        <li><a href="portal.php">Notifications<span class="badge red">16</span></a></li>
+
+
+        <?php if ($_SESSION['username'] == labeeb || $_SESSION['username'] == m_mcmillan || $_SESSION['username'] == aziz || $_SESSION['username'] == aman): ?>
+          <li><a href="settings.php">Settings</a></li><!-- <span class="badge yellow">15</span> -->
+        <?php endif ?>
+
+
+        <li><a href="logout.php">Logout</a></li> <!-- Destroy session variables once logout is hit. Also implement auto-login? from Login Tab in Index -->
+      </ul>
+    </nav>
+
+  <div class="container">
+      <div class="well form-horizontal" style="color: white;
+                                               text-align: center; 
+                                               font-size: 17px; 
+                                               background-color: white;" >
+  <fieldset>
+  <legend><b><span style="color: black;">Announcements</span></b></legend>
+
+    <p style="text-align: left; font-size: 18px; color: black;">
+      <b>February 2, 2017</b>
+      <br>
+      <br>
+      Welcome to the instructor portal <span class="user"><?= $_SESSION['username'] ?></span>!
+    </p>
+
+  </fieldset>
+
+
+  </div>
+      </div><!-- /.container -->
+
+
+  <div class="container">
+      <div class="well form-horizontal" style="color: white;
+                                               text-align: left; 
+                                               font-size: 17px; 
+                                               background-color: white;
+                                               width: 350px;" >
+  <fieldset>
+  <legend><b><span style="color: black;">Summary</span></b></legend>
+
+    <p style="text-align: left; font-size: 18px; color: black;">
+      <b>February 2, 2017</b>
+      <br>
+      <br>
+      
+    </p>
+
+  </fieldset>
+
+
+  </div>
+      </div><!-- /.container -->
+
+
+
+
+
+  <div class="table-responsive-vertical shadow-z-1">
+  <!-- Table starts here -->
+  <table id="table" class="table table-hover table-mc-light-blue">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Link</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td data-title="ID">1</td>
+          <td data-title="Name">Material Design Color Palette</td>
+          <td data-title="Link">
+            <a href="https://github.com/zavoloklom/material-design-color-palette" target="_blank">GitHub</a>
+          </td>
+          <td data-title="Status">Completed</td>
+        </tr>
+        <tr>
+          <td data-title="ID">2</td>
+          <td data-title="Name">Material Design Iconic Font</td>
+          <td data-title="Link">
+            <a href="http://codepen.io/zavoloklom/pen/uqCsB" target="_blank">Codepen</a>
+            <a href="https://github.com/zavoloklom/material-design-iconic-font" target="_blank">GitHub</a>
+          </td>
+          <td data-title="Status">Completed</td>
+        </tr>
+        <tr>
+          <td data-title="ID">3</td>
+          <td data-title="Name">Material Design Hierarchical Display</td>
+          <td data-title="Link">
+            <a href="http://codepen.io/zavoloklom/pen/eNaEBM" target="_blank">Codepen</a>
+            <a href="https://github.com/zavoloklom/material-design-hierarchical-display" target="_blank">GitHub</a>
+          </td>
+          <td data-title="Status">Completed</td>
+        </tr>
+        <tr>
+          <td data-title="ID">4</td>
+          <td data-title="Name">Material Design Sidebar</td>
+          <td data-title="Link"><a href="http://codepen.io/zavoloklom/pen/dIgco" target="_blank">Codepen</a></td>
+          <td data-title="Status">Completed</td>
+        </tr>
+        <tr>
+          <td data-title="ID">5</td>
+          <td data-title="Name">Material Design Tiles</td>
+          <td data-title="Link">
+            <a href="http://codepen.io/zavoloklom/pen/wtApI" target="_blank">Codepen</a>
+          </td>
+          <td data-title="Status">Completed</td>
+        </tr>
+        <tr>
+          <td data-title="ID">6</td>
+          <td data-title="Name">Material Design Typography</td>
+          <td data-title="Link">
+            <a href="http://codepen.io/zavoloklom/pen/IkaFL" target="_blank">Codepen</a>
+            <a href="https://github.com/zavoloklom/material-design-typography" target="_blank">GitHub</a>
+          </td>
+          <td data-title="Status">Completed</td>
+        </tr>
+        <tr>
+          <td data-title="ID">7</td>
+          <td data-title="Name">Material Design Buttons</td>
+          <td data-title="Link">
+            <a href="http://codepen.io/zavoloklom/pen/Gubja" target="_blank">Codepen</a>
+          </td>
+          <td data-title="Status">In progress</td>
+        </tr>
+        <tr>
+          <td data-title="ID">8</td>
+          <td data-title="Name">Material Design Form Elements</td>
+          <td data-title="Link">
+            <a href="http://codepen.io/zavoloklom/pen/yaozl" target="_blank">Codepen</a>
+          </td>
+          <td data-title="Status">In progress</td>
+        </tr>
+        <tr>
+          <td data-title="ID">9</td>
+          <td data-title="Name">Material Design Email Template</td>
+          <td data-title="Link">
+            <a href="http://codepen.io/zavoloklom/pen/qEVqzx" target="_blank">Codepen</a>
+          </td>
+          <td data-title="Status">Completed</td>
+        </tr>
+        <tr>
+          <td data-title="ID">10</td>
+          <td data-title="Name">Material Design Animation Timing (old one)</td>
+          <td data-title="Link">
+            <a href="http://codepen.io/zavoloklom/pen/Jbrho" target="_blank">Codepen</a>
+          </td>
+          <td data-title="Status">Completed</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+
+
+  <script type="text/javascript">
+
+    /* Client Table JavaScript */
+
+    $(document).ready(function() {
+
+        var table = $('#table');
+        
+        // Table bordered
+        $('#table-bordered').change(function() {
+            var value = $( this ).val();
+            table.removeClass('table-bordered').addClass(value);
+        });
+
+        // Table striped
+        $('#table-striped').change(function() {
+            var value = $( this ).val();
+            table.removeClass('table-striped').addClass(value);
+        });
+      
+        // Table hover
+        $('#table-hover').change(function() {
+            var value = $( this ).val();
+            table.removeClass('table-hover').addClass(value);
+        });
+
+        // Table color
+        $('#table-color').change(function() {
+            var value = $(this).val();
+            table.removeClass(/^table-mc-/).addClass(value);
+        });
+    });
+
+    // jQuery’s hasClass and removeClass on steroids
+    // by Nikita Vasilyev
+    // https://github.com/NV/jquery-regexp-classes
+    (function(removeClass) {
+      jQuery.fn.removeClass = function( value ) {
+        if ( value && typeof value.test === "function" ) {
+          for ( var i = 0, l = this.length; i < l; i++ ) {
+            var elem = this[i];
+            if ( elem.nodeType === 1 && elem.className ) {
+              var classNames = elem.className.split( /\s+/ );
+
+              for ( var n = classNames.length; n--; ) {
+                if ( value.test(classNames[n]) ) {
+                  classNames.splice(n, 1);
+                }
+              }
+              elem.className = jQuery.trim( classNames.join(" ") );
+            }
+          }
+        } else {
+          removeClass.call(this, value);
+        }
+        return this;
+      }
+    })(jQuery.fn.removeClass);
+
+  </script>
+
+
+
+  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</body>
+
+  <!--
+  <section class="about">
+    <p class="about-links">
+      <a href="http://www.cssflow.com/snippets/menu-with-notification-badges" target="_parent">View Article</a>
+      <a href="http://www.cssflow.com/snippets/menu-with-notification-badges.zip" target="_parent">Download</a>
+    </p>
+    <p class="about-author">
+      &copy; 2016&ndash;2017 <a href="portal.php" target="_blank">STEM Academy</a> -
+      <a href="http://www.cssflow.com/mit-license" target="_blank">MIT License</a><br>
+      Original PSD by <a href="http://www.premiumpixels.com/freebies/menu-notification-badges-psd/" target="_blank">Orman Clark</a>
+    </p>
+  </section>
+  -->
+
+</html>
