bc56ed0c03e2a548c58df11e65a22d80673af85f
labeebk1 <labeebk1@gmail.com>
2017-02-15 00:36:37 -0500

    test

--- test.php
+++ test.php
@@ -46,50 +46,261 @@
     </nav>
 
 
+      <div class="wrapper">
+  
+  <div class="table">
     
-<table class="responstable">
+    <div class="row header">
+      <div class="cell">
+        Name
+      </div>
+      <div class="cell">
+        Age
+      </div>
+      <div class="cell">
+        Occupation
+      </div>
+      <div class="cell">
+        Location
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        Luke Peters
+      </div>
+      <div class="cell">
+        25
+      </div>
+      <div class="cell">
+        Freelance Web Developer
+      </div>
+      <div class="cell">
+        Brookline, MA
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        Joseph Smith
+      </div>
+      <div class="cell">
+        27
+      </div>
+      <div class="cell">
+        Project Manager
+      </div>
+      <div class="cell">
+        Somerville, MA
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        Maxwell Johnson
+      </div>
+      <div class="cell">
+        26
+      </div>
+      <div class="cell">
+        UX Architect & Designer
+      </div>
+      <div class="cell">
+        Arlington, MA
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        Harry Harrison
+      </div>
+      <div class="cell">
+        25
+      </div>
+      <div class="cell">
+        Front-End Developer
+      </div>
+      <div class="cell">
+        Boston, MA
+      </div>
+    </div>
+    
+  </div>
   
-  <tr>
-    <th>Main driver</th>
-    <th data-th="Driver details"><span>First name</span></th>
-    <th>Surname</th>
-    <th>Date of birth</th>
-    <th>Relationship</th>
-  </tr>
+  <div class="table">
+    
+    <div class="row header green">
+      <div class="cell">
+        Product
+      </div>
+      <div class="cell">
+        Unit Price
+      </div>
+      <div class="cell">
+        Quantity
+      </div>
+      <div class="cell">
+        Date Sold
+      </div>
+      <div class="cell">
+        Status
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        Solid oak work table
+      </div>
+      <div class="cell">
+        $800
+      </div>
+      <div class="cell">
+        10
+      </div>
+      <div class="cell">
+        03/15/2014
+      </div>
+      <div class="cell">
+        Waiting for Pickup
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        Leather iPhone wallet
+      </div>
+      <div class="cell">
+        $45
+      </div>
+      <div class="cell">
+        120
+      </div>
+      <div class="cell">
+        02/28/2014
+      </div>
+      <div class="cell">
+        In Transit
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        27" Apple Thunderbolt displays
+      </div>
+      <div class="cell">
+        $1000
+      </div>
+      <div class="cell">
+        25
+      </div>
+      <div class="cell">
+        02/10/2014
+      </div>
+      <div class="cell">
+        Delivered
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        Bose studio headphones
+      </div>
+      <div class="cell">
+        $60
+      </div>
+      <div class="cell">
+        90
+      </div>
+      <div class="cell">
+        01/14/2014
+      </div>
+      <div class="cell">
+        Delivered
+      </div>
+    </div>
+    
+  </div>
   
-  <tr>
-    <td><input type="radio"/></td>
-    <td>Steve</td>
-    <td>Foo</td>
-    <td>01/01/1978</td>
-    <td>Policyholder</td>
-  </tr>
+  <div class="table">
+    
+    <div class="row header blue">
+      <div class="cell">
+        Username
+      </div>
+      <div class="cell">
+        Email
+      </div>
+      <div class="cell">
+        Password
+      </div>
+      <div class="cell">
+        Active
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        ninjalug
+      </div>
+      <div class="cell">
+        misterninja@hotmail.com
+      </div>
+      <div class="cell">
+        ************
+      </div>
+      <div class="cell">
+        Yes
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        jsmith41
+      </div>
+      <div class="cell">
+        joseph.smith@gmail.com
+      </div>
+      <div class="cell">
+        ************
+      </div>
+      <div class="cell">
+        No
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        1337hax0r15
+      </div>
+      <div class="cell">
+        hackerdude1000@aol.com
+      </div>
+      <div class="cell">
+        ************
+      </div>
+      <div class="cell">
+        Yes
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="cell">
+        hairyharry19
+      </div>
+      <div class="cell">
+        harryharry@gmail.com
+      </div>
+      <div class="cell">
+        ************
+      </div>
+      <div class="cell">
+        Yes
+      </div>
+    </div>
+    
+  </div>
   
-  <tr>
-    <td><input type="radio"/></td>
-    <td>Steffie</td>
-    <td>Foo</td>
-    <td>01/01/1978</td>
-    <td>Spouse</td>
-  </tr>
-  
-  <tr>
-    <td><input type="radio"/></td>
-    <td>Stan</td>
-    <td>Foo</td>
-    <td>01/01/1994</td>
-    <td>Son</td>
-  </tr>
-  
-  <tr>
-    <td><input type="radio"/></td>
-    <td>Stella</td>
-    <td>Foo</td>
-    <td>01/01/1992</td>
-    <td>Daughter</td>
-  </tr>
-  
-</table>
+</div>
 
 
 
