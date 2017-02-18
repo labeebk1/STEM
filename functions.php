051e13a51a5deda85865beb63b90d7348886bbfe
labeebk1 <labeebk1@gmail.com>
2017-02-18 17:39:32 -0500

    test

--- functions.php
+++ functions.php
@@ -94,7 +94,7 @@ function getCalender($year = '',$month = '')
         </div>
 
 
-        <div class="container" style="position: relative; left: 50%; transform: translateX(-50%); display: inline-block; width: 640px;">
+        <div class="container" style="position: relative; left: 50%; transform: translateX(-50%); display: inline-block; width: 694px;">
 		      <div class="well form-horizontal">
 		  		<fieldset>
 
