0c8956467aa869f27b93234003cdfb0329d53deb
labeebk1 <labeebk1@gmail.com>
2017-02-18 17:17:19 -0500

    test

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function getCalender($year = '',$month = '')
 		<h2 style="background-color: white;">
 
 
-		  <div class="container" style="left: 50%; transform: translateX(-50%);">
+		  <div class="container" style="left: 50%;">
 		      <div class="well form-horizontal" style="text-align: center; color: white; font-size: 20px; 
 		                                               background-color: #00bfff; display: inline-block;" >
 		  		<fieldset>
