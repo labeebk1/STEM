0b9e920b4e12b73ca8f5fe5591727d13054e11c7
labeebk1 <labeebk1@gmail.com>
2017-04-19 02:48:13 -0400

    test

--- portal.php
+++ portal.php
@@ -342,7 +342,7 @@
 
 		} else {
       		
-      		$strQuery = "SELECT a.Date FROM (
+      		$strQuery = "SELECT a.Date, b.Hours FROM (
     select curdate() - INTERVAL (a.a + (10 * b.a) + (100 * c.a)) DAY as Date
     from (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as a
     cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as b
